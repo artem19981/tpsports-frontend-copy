d3162b78221b2a60cd6f88efc40520421a30b788
Artem <dino.olimp@gmail.com>
2025-04-05 12:07:50 +0400

    feat: add redirect button in chat

--- src/entities/chat/config.ts
+++ src/entities/chat/config.ts
@@ -1,2 +1,12 @@
+import { ChatVariant } from 'features/Chat/model';
+import { ChatType } from './model/ChatType';
+
 export const NEW_GPT_MESSAGE_ID = 'new_gpt_message_id';
 export const NEW_USER_MESSAGE_ID = 'NEW_USER_MESSAGE_ID';
+
+export const assistantByChatVariant: Record<ChatVariant, string> = {
+  [ChatType.Trainer]: 'Спорт',
+  [ChatType.Doctor]: 'Здоровье',
+  [ChatType.Nutritionolog]: 'Питание',
+  [ChatType.Psychologist]: 'Сознание',
+};
